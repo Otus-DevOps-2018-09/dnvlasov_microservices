b645cfb1bc5110777e60f3bf59659c8186e2d73c
Dmitrii Vlasov <gcp@moslex.ru>
2018-12-11 17:03:55 +0300

    add pipeline definition

--- docker-monolith/docker-machine_start.sh
+++ docker-monolith/docker-machine_start.sh
@@ -4,4 +4,4 @@ docker-machine create --driver google \
 --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
 --google-machine-type n1-standard-1 \
 --google-zone europe-west1-b \
-docker-host
+gitlab-ci
