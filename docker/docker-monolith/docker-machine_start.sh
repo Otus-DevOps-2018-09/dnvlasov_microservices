dba4b329f4ba2ec2c97ed338d9ac0b32d2f4444a
Dmitrii Vlasov <gcp@moslex.ru>
2019-01-05 15:40:05 +0300

    Add .env.example

---
+++ docker/docker-monolith/docker-machine_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export GOOGLE_PROJECT=docker-224713
+docker-machine create --driver google \
+--google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
+--google-machine-type n1-standard-1 \
+--google-zone europe-west1-b \
+--google-disk-size 100 \
+docker-host
